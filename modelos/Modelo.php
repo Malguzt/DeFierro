fbaff9d52d848869655b75abfcc381fe7ef831ed
Malguzt <malguzt@gmail.com>
2009-11-28 23:40:15 -0300

    Primer Commit

---
+++ modelos/Modelo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Clase generica con las caracteristicas comunes a todos los modelo.
+ *
+ * @author Lenscak José Francisco
+ */
+abstract class Modelo {
+    protected $tabla;
+
+    public function __construct(){
+
+    }
+}
+?>
